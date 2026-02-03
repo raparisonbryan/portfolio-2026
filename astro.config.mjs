236ba4e822d04e0a874a80383034eb6cc8bf251f
raparisonbryan <raparisonbryan@yahoo.fr>
2026-02-03 10:25:49 +0100

    feat : init resend & fix style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://your-domain.com",
+  site: "https://raparisonbryan.fr",
   integrations: [react()],
-  output: "static", // SSG par défaut
+  output: "server",
+  adapter: node({ mode: "standalone" }),
   build: {
     inlineStylesheets: "auto",
   },
