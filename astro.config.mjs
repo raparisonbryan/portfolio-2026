c1ba7aa6ebac64a3878067144cab41b91ec031b0
raparisonbryan <raparisonbryan@yahoo.fr>
2026-02-03 12:23:23 +0100

    fix build for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://raparisonbryan.fr",
   integrations: [react()],
   output: "server",
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
   build: {
     inlineStylesheets: "auto",
   },
